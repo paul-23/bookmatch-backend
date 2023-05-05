0a1fb076fefd4c50a5b0da11e051e245058490d5
ArnauLeinster <arnau.leinster@gmail.com>
2023-05-05 10:25:57 +0200

    BooksDtoFirst

--- BookMatch_Backend/src/main/java/com/redarpa/bookmatch/dto/User.java
+++ BookMatch_Backend/src/main/java/com/redarpa/bookmatch/dto/User.java
@@ -3,10 +3,112 @@
  */
 package com.redarpa.bookmatch.dto;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
 /**
  * @author Marc
  *
  */
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_user;
+	@Column(name = "username")
+	private String username;
+	@Column(name = "email")
+	private String email;
+	@Column(name = "pass")
+	private String pass;
+
+	@OneToMany
+	@JoinColumn(name = "id_book")
+	private List<Book> book;
+	
+	@OneToMany
+	@JoinColumn(name = "id_message")
+	private List<Message> message;
+
+	public User() {
+
+	}
+
+	public User(Long id_user, String username, String email, String pass, List<Book> book, List<Message> message) {
+		super();
+		this.id_user = id_user;
+		this.username = username;
+		this.email = email;
+		this.pass = pass;
+		this.book = book;
+		this.message = message;
+	}
+
+	public Long getId_user() {
+		return id_user;
+	}
+
+	public void setId_user(Long id_user) {
+		this.id_user = id_user;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Books")
+	public List<Book> getBook() {
+		return book;
+	}
+
+	public void setBook(List<Book> book) {
+		this.book = book;
+	}
+
+	@JsonIgnore
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "Messages")
+	public List<Message> getMessage() {
+		return message;
+	}
+
+	public void setMessage(List<Message> message) {
+		this.message = message;
+	}
+
+	
+	
 }
