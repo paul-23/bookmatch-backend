ae24a71e9f1abb7c40fe0e6522960676823b558b
burnout131 <marcrg07@gmail.com>
2023-05-18 18:45:18 +0200

    Added security features

---
+++ BookMatch_Backend/src/main/java/com/redarpa/bookmatch/user/UserRecord.java
@@ -0,0 +1,3 @@
+package com.redarpa.bookmatch.user;
+
+public record UserRecord(Long id, String firstName, String lastName, String email, String password){}
---
+++ BookMatch_Backend/src/main/java/com/redarpa/bookmatch/user/UserService.java
@@ -0,0 +1,65 @@
+package com.redarpa.bookmatch.user;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.redarpa.bookmatch.exception.UserAlreadyExistsException;
+import com.redarpa.bookmatch.exception.UserNotFoundException;
+
+/**
+ * @author Jose Marin
+ */
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UserService implements IUserService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public User add(User user) {
+        Optional<User> theUser = userRepository.findByEmail(user.getEmail());
+        if (theUser.isPresent()){
+            throw new UserAlreadyExistsException("A user with " +user.getEmail() +" already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public List<UserRecord> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(user -> new UserRecord(
+                        user.getId(),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getEmail(),
+                		user.getPassword())).collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public void delete(String email) {
+        userRepository.deleteByEmail(email);
+    }
+
+    @Override
+    public User getUser(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    @Override
+    public User update(User user) {
+        user.setRoles(user.getRoles());
+        return userRepository.save(user);
+    }
+}
