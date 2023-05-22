98685103a57ce1e84ea23075ea89a99709644729
Paul S <62121921+paul-23@users.noreply.github.com>
2023-05-23 00:24:57 +0200

    Signup with profile image

--- BookMatch_Backend/src/main/java/com/redarpa/bookmatch/controller/AuthController.java
+++ BookMatch_Backend/src/main/java/com/redarpa/bookmatch/controller/AuthController.java
@@ -3,7 +3,14 @@
  */
 package com.redarpa.bookmatch.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,7 +22,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.redarpa.bookmatch.dao.IUserDAO;
 import com.redarpa.bookmatch.dto.JwtResponse;
@@ -24,6 +33,7 @@ import com.redarpa.bookmatch.dto.MessageResponse;
 import com.redarpa.bookmatch.dto.SignupRequest;
 import com.redarpa.bookmatch.dto.User;
 import com.redarpa.bookmatch.service.UserDetailsImpl;
+import com.redarpa.bookmatch.service.UserServiceImp;
 import com.redarpa.bookmatch.utils.JwtUtils;
 
 import jakarta.validation.Valid;
@@ -50,6 +60,9 @@ public class AuthController {
 
 	@Autowired
 	JwtUtils jwtUtils;
+	
+	@Autowired
+	UserServiceImp userServiceImpl;
 
 	// Post mapping to sign in and get the token
 	@PostMapping("/signin")
@@ -72,21 +85,51 @@ public class AuthController {
 
 	// Signup to reguster a new user
 	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest, @RequestParam(value = "image", required = false) MultipartFile imageFile) {
 
 	    // Checks email is available
 	    if (userRepository.existsByEmail(signUpRequest.getEmail())) {
 	        return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use!"));
 	    }
 
-	    // Create new user's account
-	    User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
-	            encoder.encode(signUpRequest.getPassword()));
+	    try {
+	        // Create new user's account
+	        User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
+	                encoder.encode(signUpRequest.getPassword()));
 
-	    // Sets roles and saves user
-	    user.setRoleId("ROLE_USER");
-	    userRepository.save(user);
+	        // Sets roles and saves user
+	        user.setRoleId("ROLE_USER");
+	        userRepository.save(user);
 
-	    return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	        if (imageFile != null && !imageFile.isEmpty()) {
+	            userServiceImpl.saveUserWithImage(user, imageFile.getBytes());
+	        } else {
+	            saveImg(user.getId_user());
+	        }
+
+	        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	    } catch (IOException ex) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new MessageResponse("Error saving user image: " + ex.getMessage()));
+	    }
+	}
+	
+	public void saveImg(Long id) {
+
+		try {
+			String defaultImagePath = "default_user.jpg";
+
+			BufferedImage defaultImage = ImageIO.read(getClass().getClassLoader().getResource(defaultImagePath));
+
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			ImageIO.write(defaultImage, "jpg", outputStream);
+			byte[] defaultImageBytes = outputStream.toByteArray();
+
+			User userById = userServiceImpl.userById(id);
+			userById.setProfile_image(defaultImageBytes);
+
+			userServiceImpl.saveImage(userById);
+		} catch (Exception ex) {
+			System.out.println("Ocurrió un error al asignar la imagen por defecto: " + ex.getMessage());
+		}
 	}
 }
