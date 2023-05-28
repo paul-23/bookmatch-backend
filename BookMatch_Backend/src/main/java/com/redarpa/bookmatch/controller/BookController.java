dd5042936bcf41e72f31c34a06748e4a4b6d3af5
Paul S <62121921+paul-23@users.noreply.github.com>
2023-05-28 02:36:11 +0200

    Book post and put

--- BookMatch_Backend/src/main/java/com/redarpa/bookmatch/controller/BookController.java
+++ BookMatch_Backend/src/main/java/com/redarpa/bookmatch/controller/BookController.java
@@ -21,13 +21,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.redarpa.bookmatch.dao.IBookDAO;
 import com.redarpa.bookmatch.dto.Book;
 import com.redarpa.bookmatch.service.BookServiceImp;
@@ -66,19 +66,22 @@ public class BookController {
 		return bookServiceImp.listAllBooks();
 	}
 	
-	@PostMapping(value = "/book", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PostMapping(value = "/book")
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 	public Book saveBook(@RequestParam(value = "image", required = false) MultipartFile imageFile,
-			@RequestPart("book") Book book) throws IOException {
-		if (imageFile != null && !imageFile.isEmpty()) {
-			byte[] imageBytes = imageFile.getBytes();
-			Book savedBook = bookServiceImp.saveBookWithImage(book, imageBytes);
-			return savedBook;
-		} else {
-			Book savedBook = bookServiceImp.saveBook(book);
-			saveCoverByBookISBN(savedBook.getId_book(), savedBook.getIsbn());
-			return savedBook;
-		}
+	                     @RequestPart("book") String bookJson) throws IOException {
+	    ObjectMapper objectMapper = new ObjectMapper();
+	    Book book = objectMapper.readValue(bookJson, Book.class);
+
+	    if (imageFile != null && !imageFile.isEmpty()) {
+	        byte[] imageBytes = imageFile.getBytes();
+	        Book savedBook = bookServiceImp.saveBookWithImage(book, imageBytes);
+	        return savedBook;
+	    } else {
+	        Book savedBook = bookServiceImp.saveBook(book);
+	        saveCoverByBookISBN(savedBook.getId_book(), savedBook.getIsbn());
+	        return savedBook;
+	    }
 	}
 
 	@GetMapping("/book/{id}")
@@ -120,85 +123,88 @@ public class BookController {
 	} */
 
 	public void saveCoverByBookISBN(Long id, String isbn) {
-		try {
-			String url = OPEN_LIBRARY_API.replace("ISBN_NUMBER", isbn);
-			URL imageUrl = new URL(url);
-			BufferedImage bufferedImage = ImageIO.read(imageUrl);
+	    try {
+	        String url = OPEN_LIBRARY_API.replace("ISBN_NUMBER", isbn);
+	        URL imageUrl = new URL(url);
+	        BufferedImage bufferedImage = ImageIO.read(imageUrl);
 
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-			ImageIO.write(bufferedImage, "jpg", outputStream);
-			byte[] imageBytes = outputStream.toByteArray();
+	        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	        ImageIO.write(bufferedImage, "jpg", outputStream);
+	        byte[] imageBytes = outputStream.toByteArray();
 
-			Book bookById = new Book();
-			bookById = bookServiceImp.bookById(id);
-			bookById.setCover_image(imageBytes);
+	        Book bookById = bookServiceImp.bookById(id);
+	        bookById.setCover_image(imageBytes);
 
-			bookServiceImp.saveImage(bookById);
+	        bookServiceImp.saveImage(bookById);
 
-		} catch (Exception e) {
-			try {
-				String defaultImagePath = "default_cover.jpg";
+	    } catch (Exception e) {
+	        try {
+	            String defaultImagePath = "default_cover.jpg";
 
-				BufferedImage defaultImage = ImageIO.read(getClass().getClassLoader().getResource(defaultImagePath));
+	            BufferedImage defaultImage = ImageIO.read(getClass().getClassLoader().getResource(defaultImagePath));
 
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				ImageIO.write(defaultImage, "jpg", outputStream);
-				byte[] defaultImageBytes = outputStream.toByteArray();
+	            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+	            ImageIO.write(defaultImage, "jpg", outputStream);
+	            byte[] defaultImageBytes = outputStream.toByteArray();
 
-				Book bookById = bookServiceImp.bookById(id);
-				bookById.setCover_image(defaultImageBytes);
+	            Book bookById = bookServiceImp.bookById(id);
+	            bookById.setCover_image(defaultImageBytes);
 
-				bookServiceImp.saveImage(bookById);
-			} catch (Exception ex) {
-				System.out.println("Ocurrió un error al asignar la imagen por defecto: " + ex.getMessage());
-			}
-		}
+	            bookServiceImp.saveImage(bookById);
+	        } catch (Exception ex) {
+	            System.out.println("Ocurrió un error al asignar la imagen por defecto: " + ex.getMessage());
+	        }
+	    }
 	}
 
-	@PutMapping(value = "/book/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	@PutMapping(value = "/book/{id}")
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 	public Book updateBook(@PathVariable(name = "id") Long id,
-			@RequestParam(value = "image", required = false) MultipartFile imageFile, @RequestPart("book") Book book)
-			throws IOException {
+	                       @RequestPart(value = "image", required = false) MultipartFile imageFile,
+	                       @RequestPart("book") String bookJson) throws IOException {
 
-		Book selectedBook = bookServiceImp.bookById(id);
+	    Book selectedBook = bookServiceImp.bookById(id);
 
-		// Verificar si el usuario actual es el creador del libro
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (authentication != null && authentication.getPrincipal() instanceof UserDetailsImpl) {
-			UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-			Long userId = userDetails.getId();
+	    // Verificar si el usuario actual es el creador del libro
+	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    if (authentication != null && authentication.getPrincipal() instanceof UserDetailsImpl) {
+	        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+	        Long userId = userDetails.getId();
 
-			// Comparar el ID del usuario actual con el ID del usuario del libro
-			if (!authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
-					&& !selectedBook.getUser().getId_user().equals(userId)) {
-				throw new AccessDeniedException("No tienes permiso para modificar este libro");
-			}
-		}
+	        // Comparar el ID del usuario actual con el ID del usuario del libro
+	        if (!authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))
+	                && !selectedBook.getUser().getId_user().equals(userId)) {
+	            throw new AccessDeniedException("No tienes permiso para modificar este libro");
+	        }
+	    }
 
-		selectedBook.setAuthor(book.getAuthor());
-		selectedBook.setTitle(book.getTitle());
-		selectedBook.setIsbn(book.getIsbn());
-		selectedBook.setCategory(book.getCategory());
-		selectedBook.setAviable(book.getAviable());
-		selectedBook.setEditorial(book.getEditorial());
-		selectedBook.setDescription(book.getDescription());
+	    ObjectMapper objectMapper = new ObjectMapper();
+	    Book updatedBook = objectMapper.readValue(bookJson, Book.class);
 
-		Book updatedBook;
-		if (imageFile != null && !imageFile.isEmpty()) {
-			updatedBook = bookServiceImp.updateBookWithImage(selectedBook, imageFile.getBytes());
-		} else {
-			updatedBook = bookServiceImp.updateBook(selectedBook);
-			saveCoverByBookISBN(updatedBook.getId_book(), updatedBook.getIsbn());
-		}
+	    selectedBook.setAuthor(updatedBook.getAuthor());
+	    selectedBook.setTitle(updatedBook.getTitle());
+	    selectedBook.setIsbn(updatedBook.getIsbn());
+	    selectedBook.setCategory(updatedBook.getCategory());
+	    selectedBook.setAviable(updatedBook.getAviable());
+	    selectedBook.setEditorial(updatedBook.getEditorial());
+	    selectedBook.setDescription(updatedBook.getDescription());
 
-		if (updatedBook.getCover_image() == null || updatedBook.getCover_image().equals(null)
-				|| updatedBook.getCover_image().equals("")) {
-			saveCoverByBookISBN(updatedBook.getId_book(), updatedBook.getIsbn());
-		}
+	    if (imageFile != null && !imageFile.isEmpty()) {
+	        byte[] imageBytes = imageFile.getBytes();
+	        updatedBook = bookServiceImp.updateBookWithImage(selectedBook, imageBytes);
+	    } else {
+	        updatedBook = bookServiceImp.updateBook(selectedBook);
+	        saveCoverByBookISBN(updatedBook.getId_book(), updatedBook.getIsbn());
+	    }
 
-		return updatedBook;
+	    if (updatedBook.getCover_image() == null || updatedBook.getCover_image().equals(null)
+	            || updatedBook.getCover_image().equals("")) {
+	        saveCoverByBookISBN(updatedBook.getId_book(), updatedBook.getIsbn());
+	    }
+
+	    return updatedBook;
 	}
+
 
 	@PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')")
 	@DeleteMapping("/book/{id}")
